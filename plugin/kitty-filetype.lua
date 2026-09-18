6248b688632635ccf4307ee3f4589166ce5ff9ed
Bùi Tạ Thái Phú <167422983+phuBui930@users.noreply.github.com>
2026-09-18 20:06:14 +0700

    fix treesitter api

--- plugin/kitty-filetype.lua
+++ plugin/kitty-filetype.lua
@@ -1,14 +1,13 @@
 local has_TS_parser , TS_parser = pcall(require, "nvim-treesitter.parsers")
 -- local parser_config = pcall(require("nvim-treesitter.parsers").get_parser_configs())
 if has_TS_parser then
-  local parser_config = TS_parser.get_parser_configs()
 
   local plugin_dir = vim.fn.fnamemodify(
     debug.getinfo(1, "S").source:sub(2),
     ":p:h:h"
   )
 
-  parser_config.kitty = {
+  TS_parser.kitty = {
     install_info = {
       url = "https://github.com/OXY2DEV/tree-sitter-kitty",
       files = { "src/parser.c" },
